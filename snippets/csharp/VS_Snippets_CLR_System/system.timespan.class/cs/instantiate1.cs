dafcb67680f9726f9d22af479261793ebcd7e234
Maira Wenzel <mairaw@microsoft.com>
2019-12-16 23:33:32 -0800

    add culture info

--- snippets/csharp/VS_Snippets_CLR_System/system.timespan.class/cs/instantiate1.cs
+++ snippets/csharp/VS_Snippets_CLR_System/system.timespan.class/cs/instantiate1.cs
@@ -40,7 +40,7 @@ public class Example
       TimeSpan travelTime = arrival - departure;  
       Console.WriteLine("{0} - {1} = {2}", arrival, departure, travelTime);      
       
-      // The example displays the following output:
+      // The example displays the following output for en-us culture:
       //       6/13/2010 10:47:00 PM - 6/12/2010 6:32:00 PM = 1.04:15:00
       // </Snippet4>
    }
